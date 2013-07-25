f48664aa8d2b23693914b5b3b5deb6d71266fa94
hm <kobe6672823@qq.com>
2013-07-25 16:51:25 +0800

    change conf format and emulations times and add 5 apps

--- project/calModule/cal.py
+++ project/calModule/cal.py
@@ -10,6 +10,14 @@ from exportImg import *
 #get emulator conf
 TBF_DELAY, TBF_DELAY_TEST_START, TBF_DELAY_TEST_END, EMULATE_TIMES, NUMBER_OF_APP, CEN_PUSH_HB_START, CEN_PUSH_HB_END = readEmulatorConf()
 
+def __getRandomInt(rangeStr):
+    """get a random int from a string represents a range, the string should be in such format: 'start-end'
+    and end >= start!"""
+    
+    start = int(rangeStr.split("-")[0])
+    end = int(rangeStr.split("-")[1])
+    return random.randint(start, end)
+    
 def __initApps():
     """a method to read in conf and initial all apps according to the conf file"""
     
@@ -19,19 +27,23 @@ def __initApps():
     while (cnt < NUMBER_OF_APP):
         app = {}
         #front end conf
-        app["FEHBTypes"] = int(random.uniform(appConfs[cnt]["FEHBTypesStart"], appConfs[cnt]["FEHBTypesEnd"]+1))
+        app["FEHBTypes"] = __getRandomInt(appConfs[cnt]["FEHBTypesRange"])
         type = 0
         app["FEHBTimes"] = []
         while (type < app["FEHBTypes"]):
-            app["FEHBTimes"].append(int(random.uniform(appConfs[cnt]["FEHBTimesStart"], appConfs[cnt]["FEHBTimesEnd"]+1)))
+            tmp = random.randint(0, len(appConfs[cnt]["FEHBTimesRanges"]) - 1)
+            hbtimes = __getRandomInt(appConfs[cnt]["FEHBTimesRanges"][tmp])
+            app["FEHBTimes"].append(hbtimes)
             type += 1
         
         #back end conf
-        app["BEHBTypes"] = int(random.uniform(appConfs[cnt]["BEHBTypesStart"], appConfs[cnt]["BEHBTypesEnd"]+1))
+        app["BEHBTypes"] = __getRandomInt(appConfs[cnt]["BEHBTypesRange"])
         type = 0
         app["BEHBTimes"] = []
         while (type < app["BEHBTypes"]):
-            app["BEHBTimes"].append(int(random.uniform(appConfs[cnt]["BEHBTimesStart"], appConfs[cnt]["BEHBTimesEnd"]+1)))
+            tmp = random.randint(0, len(appConfs[cnt]["BEHBTimesRanges"]) - 1)
+            hbtimes = __getRandomInt(appConfs[cnt]["BEHBTimesRanges"][tmp])
+            app["BEHBTimes"].append(hbtimes)
             type += 1
         apps.append(app)
         cnt += 1
@@ -83,7 +95,7 @@ def __calCCCH(apps, appNumbers, cen_push_hb):
     num = 0
     while (num < appNumbers):
         #the first app needs to use the front end conf, while the other apps need to use the back end conf
-        if (num == 0):
+        if (num == 0 and appNumbers == 1):
             useFrontConf = True
         else:
             useFrontConf = False
@@ -108,12 +120,18 @@ def emulate():
     oneAppCCCHNonCenSum = 0
     ThreeAppsCCCHNonCenSum = 0
     FiveAppsCCCHNonCenSum = 0
+    SevenAppsCCCHNonCenSum = 0
+    TenAppsCCCHNonCenSum = 0
     
     #for centralized push
     oneAppCCCHCenSum = 0
     ThreeAppsCCCHCenSum = 0
     FiveAppsCCCHCenSum = 0
+    SevenAppsCCCHCenSum = 0
+    TenAppsCCCHCenSum = 0
+    print "start emulations!"
     while (emulationTimes <= EMULATE_TIMES):
+        print "%d times" % emulationTimes
         apps = __initApps()
         cen_push_hb = int(random.uniform(CEN_PUSH_HB_START, CEN_PUSH_HB_END))
         nonCenPush, cenPush = __calCCCH(apps, 1, cen_push_hb)
@@ -129,6 +147,14 @@ def emulate():
         FiveAppsCCCHNonCenSum += nonCenPush
         FiveAppsCCCHCenSum += cenPush
         
+        nonCenPush, cenPush = __calCCCH(apps, 7, cen_push_hb)
+        SevenAppsCCCHNonCenSum += nonCenPush
+        SevenAppsCCCHCenSum += cenPush
+        
+        nonCenPush, cenPush = __calCCCH(apps, 10, cen_push_hb)
+        TenAppsCCCHNonCenSum += nonCenPush
+        TenAppsCCCHCenSum += cenPush
+        
         emulationTimes += 1
     
     oneAppCCCHNonCenAvg = oneAppCCCHNonCenSum / EMULATE_TIMES
@@ -140,15 +166,25 @@ def emulate():
     FiveAppsCCCHNonCenAvg = FiveAppsCCCHNonCenSum / EMULATE_TIMES
     FiveAppsCCCHCenAvg = FiveAppsCCCHCenSum / EMULATE_TIMES
     
+    SevenAppsCCCHNonCenAvg = SevenAppsCCCHNonCenSum / EMULATE_TIMES
+    SevenAppsCCCHCenAvg = SevenAppsCCCHCenSum / EMULATE_TIMES
+    
+    TenAppsCCCHNonCenAvg = TenAppsCCCHNonCenSum / EMULATE_TIMES
+    TenAppsCCCHCenAvg = TenAppsCCCHCenSum / EMULATE_TIMES
+    
     print "------------------Non centralized push averages-------------"
     print "oneAppCCCHNonCenAvg: %d" % oneAppCCCHNonCenAvg
     print "ThreeAppsCCCHNonCenAvg: %d" % ThreeAppsCCCHNonCenAvg
     print "FiveAppsCCCHNonCenAvg: %d" % FiveAppsCCCHNonCenAvg
+    print "SevenAppsCCCHNonCenAvg: %d" % SevenAppsCCCHNonCenAvg
+    print "TenAppsCCCHNonCenAvg: %d" % TenAppsCCCHNonCenAvg
     
     print "------------------Centralized push averages-------------"
     print "oneAppCCCHCenAvg: %d" % oneAppCCCHCenAvg
     print "ThreeAppsCCCHCenAvg: %d" % ThreeAppsCCCHCenAvg
     print "FiveAppsCCCHCenAvg: %d" % FiveAppsCCCHCenAvg
+    print "SevenAppsCCCHCenAvg: %d" % SevenAppsCCCHCenAvg
+    print "TenAppsCCCHCenAvg: %d" % TenAppsCCCHCenAvg
     
-    exportCCCHAvgToPNG([oneAppCCCHNonCenAvg, ThreeAppsCCCHNonCenAvg, FiveAppsCCCHNonCenAvg], 
-        [oneAppCCCHCenAvg, ThreeAppsCCCHCenAvg, FiveAppsCCCHCenAvg])
+    exportCCCHAvgToPNG([oneAppCCCHNonCenAvg, ThreeAppsCCCHNonCenAvg, FiveAppsCCCHNonCenAvg, SevenAppsCCCHNonCenAvg, TenAppsCCCHNonCenAvg], 
+        [oneAppCCCHCenAvg, ThreeAppsCCCHCenAvg, FiveAppsCCCHCenAvg, SevenAppsCCCHCenAvg, TenAppsCCCHCenAvg])
